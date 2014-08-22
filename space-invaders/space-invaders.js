2c9a82216cd4a97bd4bb2b9c40f95dbb0a6d2222
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-08-22 15:09:30 -0400

    Make isColliding return false if edges overlapping

--- space-invaders/space-invaders.js
+++ space-invaders/space-invaders.js
@@ -194,10 +194,10 @@
   var isColliding = function(b1, b2) {
     return !(
       b1 === b2 ||
-        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
-        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
-        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
-        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+        b1.center.x + b1.size.x / 2 <= b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 <= b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 >= b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 >= b2.center.y + b2.size.y / 2
     );
   };
 
